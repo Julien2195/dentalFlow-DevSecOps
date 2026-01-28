ff2cb9758a6a4a765003a9a334360b1c3cf56a79
Max <max-94400@hotmail.fr>
2026-01-28 11:22:01 +0100

    ajout fichiers

---
+++ backend/dentaflow-app/src/main/java/org/example/dentaflowapp/DentaflowAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.dentaflowapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentaflowAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentaflowAppApplication.class, args);
+    }
+
+}
---
+++ backend/dentaflow-app/src/test/java/org/example/dentaflowapp/DentaflowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dentaflowapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentaflowAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
